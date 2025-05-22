716d7e9d020b18b23129436fa4c0baeb60869d72
Dornavineeth <vineethdorna@gmail.com>
2025-05-22 20:23:22 +0000

    fix docs and script

--- community/methods/UNDIAL/README.md
+++ community/methods/UNDIAL/README.md
@@ -4,13 +4,13 @@
 - Link​: https://arxiv.org/pdf/2402.10052
 
 # Setup
-- Hyperparameters: The original paper uses Llama-2 7B with LoRA to tune the model (rank=8, alpha=16) and learning rate of 1e-4. It's suggested to search the learning rate over [1e-5, 3e-4, 1e-4], epoch over [3,10], and use an effective batch size of 16 (batch_size * gradient_accumulation). The other important hyperparemeter is beta, the strength of penalty, which typically takes a number between [3,10,30]. If we change to other models, adjusting learning rate accordingly.
+- Hyperparameters: The original paper uses Llama-2 7B with LoRA to tune the model (rank=8, alpha=16) and learning rate of 1e-4. It's suggested to search the learning rate over [1e-5, 3e-4, 1e-4], and use an effective batch size of 32 (batch_size * gradient_accumulation). The other important hyperparemeter is beta, the strength of penalty, which typically takes a number between [3,10,30]. If we change to other models, adjusting learning rate accordingly.
 
 - Computation Setup: All experiments are run on one A100.
-- Other Details: The original paper does not use the retain set and aims to retain knowledge in all domains, not just on the retain set. So alpha is set to 0. Practionioners could search over the gemma to better retain the performance on the retain set.
+- Other Details: The original paper does not use the retain set and aims to retain knowledge in all domains, not just on the retain set. So alpha is set to 0. Practionioners could search over the alpha or gamma to better retain the performance on the retain set.
 
 # Results
-Run `run.sh` script. The results can be found in `leaderboard.md`.
+Run `run.sh` script.
 
 # Citation
 @misc{dong2024undial,
--- community/methods/UNDIAL/run.sh
+++ community/methods/UNDIAL/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export MASTER_PORT=$(python -c "import socket; s=socket.socket(); s.bind(('', 0)); print(s.getsockname()[1]); s.close()")
+echo "Master Port: $MASTER_PORT"
+
+########################################################################################################################
+########################################### Unlearn TOFU models ########################################################
+########################################################################################################################
 
 models=(
     "Llama-3.2-1B-Instruct"
@@ -8,18 +14,18 @@ trainers_experiments=(
     "UNDIAL unlearn/tofu/default.yaml"
 )
 forget_retain_splits=(
-    "forget05 retain95"
     "forget10 retain90"
+    "forget05 retain95"
     "forget01 retain99"
 )
 
-per_device_train_batch_size=4 # on two gpus would make effective batch size 32
-gradient_accumulation_steps=4
+per_device_train_batch_size=16
+gradient_accumulation_steps=2
 
 
-########################################################################################################################
-########################################### Unlearn TOFU models ########################################################
-########################################################################################################################
+lrs=(1e-5 1e-4 3e-4)
+alphas=(1 2 5)
+betas=(3 10 30)
 
 
 for split in "${forget_retain_splits[@]}"; do
@@ -29,75 +35,44 @@ for split in "${forget_retain_splits[@]}"; do
         for trainer_experiment in "${trainers_experiments[@]}"; do
             trainer=$(echo $trainer_experiment | cut -d' ' -f1)
             experiment=$(echo $trainer_experiment | cut -d' ' -f2)
-            
-            task_name=tofu_${model}_${forget_split}_${trainer} 
-            model_path=open-unlearning/tofu_${model}_full
-            echo ${task_name}: Unlearning ${model_path} using ${trainer}
+            for lr in "${lrs[@]}"; do
+                for beta in "${betas[@]}"; do 
+                    for alpha in "${alphas[@]}"; do          
+                        task_name=tofu_${model}_${forget_split}_${trainer}_lr${lr}_beta${beta}_alpha${alpha}
+                        model_path=open-unlearning/tofu_${model}_full
+                        echo ${task_name}: Unlearning ${model_path} using ${trainer}
 
-            # Unlearn
-            python src/train.py --config-name=unlearn.yaml \
-            experiment=${experiment} \
-            trainer=${trainer} \
-            task_name=${task_name} \
-            model=${model} \
-            forget_split=${forget_split} \
-            retain_split=${retain_split} \
-            model.model_args.pretrained_model_name_or_path=${model_path} \
-            retain_logs_path=saves/eval/tofu_${model}_${retain_split}/TOFU_EVAL.json \
-            trainer.args.per_device_train_batch_size=$per_device_train_batch_size \
-            trainer.args.gradient_accumulation_steps=$gradient_accumulation_steps \
+                        # Unlearn
+                        CUDA_VISIBLE_DEVICES=0 \
+                        python src/train.py --config-name=unlearn.yaml \
+                        experiment=${experiment} \
+                        trainer=${trainer} \
+                        task_name=${task_name} \
+                        model=${model} \
+                        forget_split=${forget_split} \
+                        retain_split=${retain_split} \
+                        model.model_args.pretrained_model_name_or_path=${model_path} \
+                        retain_logs_path=saves/eval/tofu_${model}_${retain_split}/TOFU_EVAL.json \
+                        trainer.args.per_device_train_batch_size=$per_device_train_batch_size \
+                        trainer.args.gradient_accumulation_steps=$gradient_accumulation_steps \
+                        trainer.args.eval_strategy=no \
+                        trainer.args.eval_on_start=False \
+                        trainer.args.learning_rate=$lr \
+                        trainer.method_args.beta=$beta \
+                        trainer.method_args.alpha=$alpha
 
-            # Eval
-            CUDA_VISIBLE_DEVICES=0 python src/eval.py \
-            experiment=eval/tofu/default.yaml \
-            forget_split=${forget_split} \
-            model=${model} \
-            task_name=${task_name} \
-            model.model_args.pretrained_model_name_or_path=saves/unlearn/${task_name} \
-            paths.output_dir=saves/unlearn/${task_name}/evals \
-            retain_logs_path=saves/eval/tofu_${model}_${retain_split}/TOFU_EVAL.json
+                        # Eval
+                        CUDA_VISIBLE_DEVICES=0 python src/eval.py \
+                        experiment=eval/tofu/default.yaml \
+                        forget_split=${forget_split} \
+                        model=${model} \
+                        task_name=${task_name} \
+                        model.model_args.pretrained_model_name_or_path=saves/unlearn/${task_name} \
+                        paths.output_dir=saves/unlearn/${task_name}/evals \
+                        retain_logs_path=saves/eval/tofu_${model}_${retain_split}/TOFU_EVAL.json
+                    done
+                done
+            done
         done
-    done
-done
-
-# #########################################################
-# #################### MUSE Unlearning ####################
-# #########################################################
-
-
-model=Llama-3.2-1B-Instruct
-
-data_splits=(
-    "News"
-    "Books"
-)
-
-trainers=(
-    "UNDIAL"
-)
-
-for data_split in "${data_splits[@]}"; do
-    for trainer in "${trainers[@]}"; do
-
-        task_name=muse_${model}_${data_split}_${trainer}
-
-        python src/train.py --config-name=unlearn.yaml \
-        experiment=unlearn/muse/default.yaml \
-        model=${model} \
-        data_split=${data_split} \
-        trainer=${trainer} \
-        task_name=${task_name} \
-        retain_logs_path=saves/eval/muse_${model}_${data_split}_retrain/MUSE_EVAL.json \
-        trainer.args.per_device_train_batch_size=${per_device_train_batch_size} \
-        trainer.args.gradient_accumulation_steps=${gradient_accumulation_steps} \
-
-        CUDA_VISIBLE_DEVICES=0 python src/eval.py \
-        experiment=eval/muse/default.yaml \
-        data_split=${data_split} \ 
-        task_name=${task_name} \
-        model=${model} \
-        model.model_args.pretrained_model_name_or_path=saves/unlearn/${task_name} \
-        paths.output_dir=saves/unlearn/${trainer}/evals \
-        retain_logs_path=saves/eval/muse_${model}_${data_split}_retrain/MUSE_EVAL.json
     done
 done
